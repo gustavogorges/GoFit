f47b6809ee9e14643afc5e03da6630a712a16198
Gustavo Gorges Koch <gustavogk@weg.net>
2023-08-07 21:52:05 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {ButtonModule} from 'primeng/button';
+import { NavComponent } from './nav/nav.component';
+import {InputTextModule} from 'primeng/inputtext';
+import { SuplementosComponent } from './suplementos/suplementos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SuplementosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
